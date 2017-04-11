dadbe45492ee2bbd1383b4470328001c9c0e7868
devhwi <zziller03@gmail.com>
2017-04-11 17:46:15 +0900

    내정보 수정

--- routes/db.js
+++ routes/db.js
@@ -52,23 +52,3 @@ module.exports = {
     });
   }
 };
-
-// https://www.npmjs.com/package/mysql#establishing-connections
-// connection.connect(function (err) {
-//   // 커넥션 오류
-//   if (err) {
-//     console.error('db 접속 중 오류 발생: ' + err.stack);
-//     return;
-//   }
-//
-//   console.log('connected as id ' + connection.threadId);
-// });
-
-// connection.query('SELECT * FROM user', function (err, rows, cols) {
-//   if (err) throw err;
-//   console.log('Test result : ', rows[0].user_id);
-//   console.log('rows', rows); // 결과 rows 정보
-//   console.log('cols', cols); // 컬럼 정보
-// });
-
-// connection.end();
--- routes/users.js
+++ routes/users.js
@@ -30,7 +30,7 @@ router.post('/login', (req, res) => {
   var pw = req.body.user_password;
 
   // password encryption
-  var shasum = crypto.createHash('sha1'); // shasum은 Hash 클래스의 인스턴스입니다.
+  var shasum = crypto.createHash('sha512'); // shasum은 Hash 클래스의 인스턴스입니다.
   shasum.update(pw);
   var encPw = shasum.digest('hex'); // 암호화 완료
 
@@ -97,9 +97,9 @@ router.post('/signup', (req, res) => {
   var user_email = req.body.user_email;
   var user_birth = req.body.user_birth;
 
-  var shasum = crypto.createHash('sha1'); // shasum은 Hash 클래스의 인스턴스입니다.
-  shasum.update(user_password);
-  user_password = shasum.digest('hex'); // 암호화 완료
+  var shasum = crypto.createHash('sha512'); // 암호화 512로 샤샤샤
+      shasum.update(user_password);
+      user_password = shasum.digest('hex'); // 암호화 완료
 
   // 쿼리 및 쿼리 파라미터
   var sql = "INSERT INTO user VALUES(?, ?, ?, ?, ?, ?)";
@@ -108,6 +108,64 @@ router.post('/signup', (req, res) => {
   db.query(sql, sqlParams).then((rows) => {
     // 성공시 메인으로 이동한다.
     res.send('<script>alert("가입 완료!");location.href="/";</script>');
+  });
+});
+
+router.get('/myInfo', (req, res) => {
+  // 사용자 아이디를 통해 세션 유무를 체크합니다
+  if(!req.session.user_id) {
+    res.send('<script>alert("로그인이 필요합니다.");location.href="/";</script>');
+  }
+  var sess = req.session;
+  var user_id = sess.user_id;
+
+  var sql = `SELECT user_id
+                  , user_name
+                  , user_phone
+                  , user_email
+                  , user_birth
+              FROM user
+              WHERE user_id = ?`;
+  var sqlParams = [user_id];
+  db.query(sql, sqlParams).then((rows) => {
+    res.render('myInfo', {
+      title: 'My Info',
+      info: rows
+    });
+  })
+});
+
+router.post('/myInfo', (req, res) => {
+  // session check
+  if(!req.session.user_id) {
+    res.send('<script>alert("로그인이 필요합니다.");location.href="/";</script>');
+  }
+  // 비밀번호 입력에 대해 확인합니다.
+  var user_password = "";
+  if(req.body.user_password !== ""
+  && req.body.user_password == req.body.user_password_confirm) {
+    user_password = req.body.user_password;
+    var shasum = crypto.createHash('sha512'); // 암호화 512로 샤샤샤
+        shasum.update(user_password);
+        user_password = shasum.digest('hex'); // 암호화 완료
+  }
+  var user_id = req.body.user_id;
+  var user_name = req.body.user_name;
+  var user_phone = req.body.user_phone;
+  var user_email = req.body.user_email;
+  var user_birth = req.body.user_birth;
+
+  var sql = `UPDATE user
+             SET user_name = ?
+               , user_phone = ?
+               , user_email = ?
+               , user_birth = ?
+             `;
+      sql+= user_password !== "" ? `, user_password = ?` : ``;
+      sql+= `WHERE user_id = ?`;
+  var sqlParams = user_password !== "" ? [user_name, user_phone, user_email, user_birth, user_password, user_id] : [user_name, user_phone, user_email, user_birth, user_id];
+  db.query(sql, sqlParams).then((rows) => {
+    res.send('<script>alert("정보가 수정되었습니다.");location.href="/user/myInfo";</script>');
   });
 });
 
