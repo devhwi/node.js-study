34fc642f59714ef3dedda5c31c4a122f8aa9f259
devhwi <zziller03@gmail.com>
2017-04-28 14:44:59 +0900

    출석현황 list view

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,33 @@ const express = require('express');
 const router = express.Router();
 const db = require('./db');
 const models = require('../models');
+const moment = require('moment');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   var sess = req.session;
-  models.user.findOne({ attributes: ['id', 'name', 'phone', 'email', 'birth']
-                      , where: { user_id: req.session.user_id }
-                      , include: { model: models.attendance } })
-  .then((rows) => {
-    res.json(rows);
-    console.log(rows);
+  var today = moment().format('YYYY-MM-DD');
+  models.user.findAndCountAll({ attributes: ['idx', 'id', 'name', 'phone', 'email', 'birth']
+                      , include: { model: models.attendance
+                                 , attributes: ['idx', 'memo',
+                                                [models.Sequelize.fn('date_format', models.Sequelize.col('submit_date'), '%Y-%m-%d %H:%i:%s'), 'submit_date']]
+                                 , where: {
+                                     date: today
+                                   }
+                      }
+  })
+  .then((result) => {
+    console.log(result.rows);
+    res.render('index', {
+      title: 'Home'
+    , user_id: sess.user_id
+    , user_name: sess.user_name
+    , moment: moment
+    , memo_result: result.rows
+    , memo_count: result.count
+    });
   }).catch((rows) => {
     // Error
-  });
-  res.render('index', {
-    title: 'Home',
-    user_id: sess.user_id,
-    user_name: sess.user_name
   });
 });
 
