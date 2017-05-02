91c6368abbeaf7c726b5aad71ed2b2220f2edd91
devhwi <zziller03@gmail.com>
2017-05-02 16:40:07 +0900

    출석 일자별 검색

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,30 @@ const models = require('../models');
 const moment = require('moment');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/:date?', (req, res, next) => {
+  // today or get
+  var getDate = req.params.date ? moment(req.params.date).format('YYYY-MM-DD') : moment().format('YYYY-MM-DD');
+  console.log(getDate);
+
+  // 날짜 체크 -> get 으로 넘어온 날짜가 오늘보다 클 수 없다.
+  if(moment(req.params.date).isAfter(moment(), 'days')) {
+    res.send('<script>alert("오늘 날짜보다 클 수 없습니다.");location.href="/"</script>');
+  }
+
   var sess = req.session;
-  var today = moment().format('YYYY-MM-DD');
   models.user.findAndCountAll({ attributes: ['idx', 'id', 'name', 'phone', 'email', 'birth']
                               , include: { model: models.attendance
-                                         , attributes: ['idx', 'memo',
-                                                        [models.Sequelize.fn('date_format', models.Sequelize.col('submit_date'), '%Y-%m-%d %H:%i:%s'), 'submit_date']]
+                                         , attributes: [ 'idx', 'memo',
+                                                         [ models.Sequelize.fn('date_format'
+                                                         , models.Sequelize.col('submit_date'), '%Y-%m-%d %H:%i:%s')
+                                                         , 'submit_date']
+                                                       ]
                                          , where: {
-                                             date: today
+                                             date: getDate
                                          }
                                          , order: ['submit_date', 'ASC']
                               }
-  })
-  .then((result) => {
-    console.log(result.rows);
+  }).then((result) => {
     res.render('index', {
       title: 'Home'
     , user_id: sess.user_id
@@ -27,9 +36,12 @@ router.get('/', (req, res, next) => {
     , moment: moment
     , memo_result: result.rows
     , memo_count: result.count
+    , date: getDate
+    , yesterday: moment(getDate).add(-1, 'days').format('YYYY-MM-DD')
+    , tomorrow: moment(getDate).add(1, 'days').format('YYYY-MM-DD')
     });
-  }).catch((rows) => {
-    // Error
+  }).catch((err) => {
+    console.log('Error ! : ' + err);
   });
 });
 
